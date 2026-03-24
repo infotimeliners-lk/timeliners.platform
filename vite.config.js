a50080ad85438ec63411b8797883e885e8796cc0
infotimeliners-lk <info.timeliners@gmail.com>
2026-03-24 21:18:19 +0530

    Update Vite config to serve HTML

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: '.',
   server: {
-    open: '/index.html'
+    host: '0.0.0.0',
+    port: 5173
   },
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: 'dist'
   }
 })
